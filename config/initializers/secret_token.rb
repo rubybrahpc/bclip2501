5b5faedc4d53b6812ae2957200ad7a3167b287af
Lukas Weinwurm <lweinwurm@hotmail.com>
2013-01-14 18:13:39 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Meinbewerbungsclip::Application.config.secret_token = 'ba5264f4526220baf8bed05cf4264d3a852d4edb4322c3d5403532db7b0434e6938460a0edda4be475b3d860b3cdbe107473862e1377a63b1cfa99d0522c5bdd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Meinbewerbungsclip::Application.config.session_store :cookie_store, key: '_meinbewerbungsclip_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Meinbewerbungsclip::Application.config.session_store :active_record_store
